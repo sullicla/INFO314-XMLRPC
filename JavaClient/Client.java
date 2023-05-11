b8823e41279c6a1db95324a8c33d131b716cd26f
sulli <sullicla@uw.edu>
2023-05-10 23:37:51 -0700

    first update

--- JavaClient/Client.java
+++ JavaClient/Client.java
@@ -4,7 +4,9 @@ import java.net.http.*;
 import javax.xml.parsers.*;
 import javax.xml.xpath.*;
 import org.w3c.dom.*;
-
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
 import org.w3c.dom.Node;
 
 /**
@@ -13,8 +15,13 @@ import org.w3c.dom.Node;
  */
 public class Client {
     private static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    private static String host;
+    private static String port;
 
     public static void main(String... args) throws Exception {
+        host = args[0];
+        port = args[1];
+
         System.out.println(add() == 0);
         System.out.println(add(1, 2, 3, 4, 5) == 15);
         System.out.println(add(2, 4) == 6);
@@ -23,26 +30,153 @@ public class Client {
         System.out.println(multiply(1, 2, 3, 4, 5) == 120);
         System.out.println(divide(10, 5) == 2);
         System.out.println(modulo(10, 5) == 0);
+
+        try {
+            System.out.print("Add: ");
+            add(1274673582, 1980109893);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+        try {
+            System.out.print("Multiply: ");
+            multiply(39165, 136582);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+        try {
+            System.out.print("Subtract: ");
+            subtract("x", "y");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+        try {
+            System.out.print("Dividing by 0: ");
+            divide(5, 0);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
     }
+
     public static int add(int lhs, int rhs) throws Exception {
-        return -1;
+        Object[] params = { lhs, rhs };
+        String response = sendRequest("add", params);
+        return parseResponse(response);
     }
+
+
     public static int add(Integer... params) throws Exception {
-        return -1;
+        String response = sendRequest("add", params);
+        return parseResponse(response);
     }
+
+
     public static int subtract(int lhs, int rhs) throws Exception {
-        return -1;
+        Object[] params = { lhs, rhs };
+        String response = sendRequest("subtract", params);
+        return parseResponse(response);
     }
+
+    public static int subtract(String lhs, String rhs) throws Exception {
+        Object[] params = { lhs, rhs };
+        String response = sendRequest("subtract", params);
+        return parseResponse(response);
+    }
+
+
     public static int multiply(int lhs, int rhs) throws Exception {
-        return -1;
+        Object[] params = { lhs, rhs };
+        String response = sendRequest("multiply", params);
+        return parseResponse(response);
     }
+
+
     public static int multiply(Integer... params) throws Exception {
-        return -1;
+        String response = sendRequest("multiply", params);
+        return parseResponse(response);
     }
+
+
     public static int divide(int lhs, int rhs) throws Exception {
-        return -1;
+        Object[] params = { lhs, rhs };
+        String response = sendRequest("divide", params);
+        return parseResponse(response);
     }
+
+
     public static int modulo(int lhs, int rhs) throws Exception {
-        return -1;
+        Object[] params = { lhs, rhs };
+        String response = sendRequest("modulo", params);
+        return parseResponse(response);
+    }
+
+    private static String sendRequest(String methodName, Object[] params) throws Exception {
+        // Create the xml formatted body
+        String xmlRequest = buildXML(methodName, params);
+
+        // build and send the rpc request
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://" + host + ":" + port + "/RPC"))
+                .header("User-Agent", "Bwongtest")
+                .header("Content-Type", "text/xml")
+                .POST(BodyPublishers.ofString(xmlRequest))
+                .build();
+
+        // Receive the response
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String xmlResponse = response.body();
+
+        return xmlResponse;
+    }
+
+    private static int parseResponse(String response) throws Exception {
+        // Create a readable xml document
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        InputStream input = new ByteArrayInputStream(response.getBytes(StandardCharsets.UTF_8));
+        Document doc = db.parse(input);
+        XPath xPath = XPathFactory.newInstance().newXPath();
+
+        // Check if there was a fault returned
+        Node faultExists = (Node) xPath.compile("/methodResponse/fault").evaluate(
+                doc, XPathConstants.NODE);
+
+        if (faultExists != null) {
+            Node faultElement = (Node) xPath.compile("/methodResponse/fault/value/struct/member/value/string").evaluate(
+                    doc, XPathConstants.NODE);
+            String fault = faultElement.getTextContent();
+
+            throw new Exception("Server threw an exception: " + fault);
+
+        } else {
+            Node resultElement = (Node) xPath.compile("/methodResponse/params/param/value").evaluate(
+                    doc, XPathConstants.NODE);
+            String result = resultElement.getTextContent();
+
+            return Integer.parseInt(result);
+        }
+    }
+
+    private static String buildXML(String methodName, Object[] params) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<?xml version=\"1.0\"?>\n");
+
+        sb.append("<methodCall>\n");
+        sb.append("<methodName>").append(methodName).append("</methodName>\n");
+        sb.append("<params>\n");
+
+        for (Object value : params) {
+            sb.append("<param>\n");
+            sb.append("<value><i4>").append(value).append("</i4></value>\n");
+            sb.append("</param>\n");
+        }
+
+        sb.append("</params>\n");
+        sb.append("</methodCall>");
+
+        return sb.toString();
     }
 }
--- JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
+++ JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
@@ -1,28 +1,259 @@
 package edu.uw.info314.xmlrpc.server;
 
+import java.io.ByteArrayInputStream;
 import java.util.*;
 import java.util.logging.*;
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 import static spark.Spark.*;
+import spark.Filter;
+import java.io.StringWriter;
+import java.io.PrintWriter;
 
 class Call {
     public String name;
-    public List<Object> args = new ArrayList<Object>();
+    public List<Integer> args = new ArrayList<Integer>();
 }
 
 public class App {
     public static final Logger LOG = Logger.getLogger(App.class.getCanonicalName());
 
     public static void main(String[] args) {
-        LOG.info("Starting up on port 4567");
+        LOG.info("Port: 8080");
+        port(8080);
 
-        // This is the mapping for POST requests to "/RPC";
-        // this is where you will want to handle incoming XML-RPC requests
-        post("/RPC", (request, response) -> { response.status(500); return "TBD"; });
+        post("/*", (request, response) -> {
+            if (!request.pathInfo().equals("/RPC")) {
+                String xmlResponse = buildXMLFault(404, "404: Not Found");
+                response.status(404);
+                response.header("Content-Type", "text/xml");
+                return xmlResponse;
+            } else {
+                try {
+                    Call call = extractXMLRPCCall(request.body());
+                    int result = 0;
+                    if (call.name.equals("add")) {
+                        result = handleAdd(call.args);
 
-        // Each of the verbs has a similar format: get() for GET,
-        // put() for PUT, delete() for DELETE. There's also an exception()
-        // for dealing with exceptions thrown from handlers.
-        // All of this is documented on the SparkJava website (https://sparkjava.com/).
+                    } else if (call.name.equals("subtract")) {
+                        result = handleSubtract(call.args);
+
+                    } else if (call.name.equals("multiply")) {
+                        result = handleMultiply(call.args);
+
+                    } else if (call.name.equals("divide")) {
+                        result = handleDivide(call.args);
+
+                    } else if (call.name.equals("modulo")) {
+                        result = handleModulo(call.args);
+                    }
+
+                    String xmlResponse = buildXML(result);
+                    response.status(200);
+                    response.header("Content-Type", "text/xml");
+                    return xmlResponse;
+
+                } catch (NumberFormatException | SAXException e) {
+                    String xmlResponse = buildXMLFault(3, "Illegal Argument Type");
+                    response.status(200);
+                    response.header("Content-Type", "text/xml");
+                    return xmlResponse;
+
+                } catch (ArithmeticException e) {
+                    String xmlResponse = buildXMLFault(1, "Divide");
+                    response.status(200);
+                    response.header("Content-Type", "text/xml");
+                    return xmlResponse;
+
+                } catch (RuntimeException e) {
+                    String xmlResponse = buildXMLFault(2, "Overflow");
+                    response.status(200);
+                    response.header("Content-Type", "text/xml");
+                    return xmlResponse;
+
+                } catch (Exception e) {
+                    StringWriter sw = new StringWriter();
+                    PrintWriter pw = new PrintWriter(sw);
+                    e.printStackTrace(pw);
+                    String sStackTrace = sw.toString(); // stack trace as a string
+                    return sStackTrace;
+                }
+            }
+        });
+
+        get("/*", (request, response) -> {
+            String xmlResponse = buildXMLFault(405, "405: Method Not Supported");
+            response.status(405);
+            response.header("Content-Type", "text/xml");
+            return xmlResponse;
+        });
+
+        put("/*", (request, response) -> {
+            String xmlResponse = buildXMLFault(405, "405: Method Not Supported");
+            response.status(405);
+            response.header("Content-Type", "text/xml");
+            return xmlResponse;
+        });
+
+        delete("/*", (request, response) -> {
+            String xmlResponse = buildXMLFault(405, "405: Method Not Supported");
+            response.status(405);
+            response.header("Content-Type", "text/xml");
+            return xmlResponse;
+        });
+    }
+
+    public static Call extractXMLRPCCall(String xmlBody) throws Exception, NumberFormatException {
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document doc = builder.parse(new ByteArrayInputStream(xmlBody.getBytes()));
+        XPath xPath = XPathFactory.newInstance().newXPath();
+
+        Node nameElement = (Node) xPath.compile("/methodCall/methodName").evaluate(
+                doc, XPathConstants.NODE);
+        String name = nameElement.getTextContent();
+
+        NodeList paramElements = (NodeList) xPath.compile("/methodCall/params/param/value").evaluate(
+                doc, XPathConstants.NODESET);
+
+        List<Integer> params = new ArrayList<Integer>();
+        for (int i = 0; i < paramElements.getLength(); i++) {
+            int param = Integer.parseInt(paramElements.item(i).getTextContent());
+            params.add(param);
+        }
+        Call call = new Call();
+        call.name = name;
+        call.args = params;
+
+        return call;
+    }
+
+    private static String buildXML(int value) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<?xml version=\"1.0\"?>\n");
+
+        sb.append("<methodResponse>\n");
+        sb.append("<params>");
+        sb.append("<param>\n");
+        sb.append("<value><i4>").append(value).append("</i4></value>\n");
+        sb.append("</param>");
+        sb.append("</params>\n");
+        sb.append("</methodResponse>");
+
+        return sb.toString();
+    }
+
+    private static String buildXMLFault(int faultCode, String faultString) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<?xml version=\"1.0\"?>\n");
+
+        sb.append("<methodResponse>\n");
+        sb.append("<fault>\n");
+        sb.append("<value>\n");
+        sb.append("<struct>\n");
+
+        sb.append("<member>\n");
+        sb.append("<name>faultCode</name>");
+        sb.append("<value><i4>").append(faultCode).append("</i4></value>\n");
+        sb.append("</member>\n");
+
+        sb.append("<member>\n");
+        sb.append("<name>faultString</name>");
+        sb.append("<value><string>").append(faultString).append("</string></value>\n");
+        sb.append("</member>\n");
+
+        sb.append("</struct>\n");
+        sb.append("</value>\n");
+        sb.append("</fault>\n");
+        sb.append("</methodResponse>");
+
+        return sb.toString();
+    }
+
+    private static int handleAdd(List<Integer> params) throws SAXException, RuntimeException {
+        long[] paramArr = new long[params.size()];
+        for (int i = 0; i < params.size(); i++) {
+            if (!(params.get(i) instanceof Integer)) {
+                throw new SAXException();
+            }
+            paramArr[i] = Long.parseLong(params.get(i).toString());
+        }
+
+        long result = 0;
+        for (long arg : paramArr) { result += arg; }
+
+        if (result > Integer.MAX_VALUE) {
+            throw new RuntimeException();
+        }
+
+        return (int) result;
+    }
+
+    private static int handleSubtract(List<Integer> params) throws SAXException {
+        if (params.size() != 2 || !(params.get(0) instanceof Integer) || !(params.get(1) instanceof Integer)) {
+            throw new SAXException();
+        }
+
+        Calc calc = new Calc();
+        int lhs = Integer.parseInt(params.get(0).toString());
+        int rhs = Integer.parseInt(params.get(1).toString());
+
+        return calc.subtract(lhs, rhs);
+    }
+
+    private static int handleMultiply(List<Integer> params) throws SAXException, RuntimeException {
+        long[] paramArr = new long[params.size()];
+        for (int i = 0; i < params.size(); i++) {
+            if (!(params.get(i) instanceof Integer)) {
+                throw new SAXException();
+            }
+            paramArr[i] = Long.parseLong(params.get(i).toString());
+        }
+
+        long result = 1;
+        for (long arg : paramArr) { result *= arg; }
+
+        if (result > Integer.MAX_VALUE) {
+            throw new RuntimeException();
+        }
+        return (int) result;
+    }
+
+    private static int handleDivide(List<Integer> params) throws SAXException, ArithmeticException {
+        if (params.size() != 2 || !(params.get(0) instanceof Integer) || !(params.get(1) instanceof Integer)) {
+            throw new SAXException();
+        }
+
+        int lhs = Integer.parseInt(params.get(0).toString());
+        int rhs = Integer.parseInt(params.get(1).toString());
+
+        if (rhs == 0) {
+            throw new ArithmeticException();
+        }
+
+        Calc calc = new Calc();
+        return calc.divide(lhs, rhs);
+    }
+
+    private static int handleModulo(List<Integer> params) throws SAXException, ArithmeticException {
+        if (params.size() != 2 || !(params.get(0) instanceof Integer) || !(params.get(1) instanceof Integer)) {
+            throw new SAXException();
+        }
+
+        int lhs = Integer.parseInt(params.get(0).toString());
+        int rhs = Integer.parseInt(params.get(1).toString());
+
+        if (rhs == 0) {
+            throw new ArithmeticException();
+        }
+
+        Calc calc = new Calc();
+        return calc.modulo(lhs, rhs);
     }
 }
