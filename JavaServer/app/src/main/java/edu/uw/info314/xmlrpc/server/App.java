892ea956e3fcea339c8dac56246b356ecb22d88c
sulli <sullicla@uw.edu>
2023-05-10 23:45:46 -0700

    updating app.java

--- JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
+++ JavaServer/app/src/main/java/edu/uw/info314/xmlrpc/server/App.java
@@ -13,7 +13,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import static spark.Spark.*;
-import spark.Filter;
 import java.io.StringWriter;
 import java.io.PrintWriter;
 
